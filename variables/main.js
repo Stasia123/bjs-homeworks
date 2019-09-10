6794792210e0af4e610f18b8a36ffeff24f21169
Anastasiya Okolovich <stasia123.86@mail.ru>
2019-09-10 17:56:43 +0300

    first commit variables by A. Okolovich

---
+++ variables/main.js
@@ -0,0 +1,24 @@
+// задание 1
+
+let algebra = 3;
+let geography = 5;
+let physics = 3;
+
+let avrMark = (algebra + geography + physics) / 3;
+
+console.log(+avrMark.toFixed(1));
+
+// задание 2
+
+let name = 'Anastasiya';
+let message = `Привет, мир! Меня зовут ${name}.`;
+
+console.log(message);
+
+// задание 3 (возможно я не уловила в чём каверзность этого задания)
+
+let x = 2;
+let y = 22;
+let z = 0;
+let result = x * y + 5 * z + x - 1;
+console.log(result);
