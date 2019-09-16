a28f3a33b7ebd9360c10404dfde6030392959aaa
Anastasiya Okolovich <stasia123.86@mail.ru>
2019-09-16 14:08:59 +0300

    ДЗ 1.2 «Расширенный синтаксис»

--- Extended-Syntax/task.js
+++ Extended-Syntax/task.js
@@ -1,4 +1,4 @@
-
+"use strict";
 
 function calculateQuadraticEquation(){
     let a = +window.a.value;
@@ -11,8 +11,21 @@ function calculateQuadraticEquation(){
 }
 
 function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    //return x;
+  let D = b**2 - 4 * a * c;
+  let x;
+  if (D > 0) {
+      alert(`Два корня, т.к. Д =  ${D}`);
+      let x1 = ((-b) - Math.sqrt(D)) / 2 * a;
+      let x2 = ((-b) + Math.sqrt(D)) / 2 * a;
+      return x = [Math.round(x1),Math.round(x2)];
+  } else if (D === 0) {
+      alert(`Один корень, т.к. Д =  ${D}`);
+      let x1 = ((-b) + Math.sqrt(D)) / 2 * a;
+      return x = [Math.round(x1)];
+  } else {
+      alert(`Корней нет, т.к. Д =  ${D}`);
+      return x = [];
+  }
 }
 
 function calculateAverageRating(){
@@ -21,10 +34,22 @@ function calculateAverageRating(){
     window.averageMark.textContent = averageMark;
 }
 
-function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    //return averageMark;
+function getAverageMark(marks) {
+  let sum = 0;
+  let averageMark;
+  
+  if (marks.length > 5) {
+    alert(`Значение посчитается для первых пяти оценок`);
+    marks = marks.slice(0,5);
+  } 
+
+  for (let i = 0; i < marks.length; i++) {
+    sum += marks[i];
+  }
+  averageMark = +(sum / marks.length).toFixed(2);
+  return averageMark;
 }
+getAverageMark(marks);
 
 function calculateDrinkTask(){
     let name = window.personName.value;
@@ -34,7 +59,10 @@ function calculateDrinkTask(){
 }
 
 function askDrink(name,dateOfBirthday){
-    // код для задачи №3 писать здесь
-    //console.log(result)
-    //return result;
+    let now = new Date().getFullYear();
+    let age = now - dateOfBirthday.getFullYear();
+    let result = (age >= 18) ? `Не желаете ли олд-фэшн, ${name}?`:
+      `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;
+    console.log(age);
+    return result;
 }
